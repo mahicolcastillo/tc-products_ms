7c0bc0e1cdd9b33b361a79ed0d43f1acbb005dbc
mvalen28 <mvalen28@externos.bancoestado.cl>
2022-10-22 19:39:20 -0300

    update main.yml

---
+++ test/unit/healthcheck/healthcheck.controller.spec.ts
@@ -0,0 +1,18 @@
+import controller               from '../../../src/components/healtcheck/healthcheck.controller';
+import { Request, Response }    from 'express';
+
+const httpMocks    = require('node-mocks-http');
+
+describe('Healthcheck Controller', () => {
+    let req: Request, res: Response;
+
+    beforeEach(() => {
+        req = httpMocks.createRequest();
+        res = httpMocks.createResponse();
+    });
+
+    test('Should response with statusCode 200', async() => {
+        const response = await controller(req, res);
+        expect(response.statusCode).toBe(200);
+    });
+});
---
+++ test/unit/healthcheck/healthcheck.route.spec.ts
@@ -0,0 +1,8 @@
+
+import router from '../../../src/components/healtcheck/healthcheck.route';
+
+describe('Healtcheck Route', () => {
+    test('Should be called the route', () => {
+        expect(router).toBeTruthy();
+    });
+});
