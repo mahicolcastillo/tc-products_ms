ffebf0638c6ce857daa62272e4484308c98fd931
mvalen28 <mvalen28@externos.bancoestado.cl>
2022-10-21 23:34:31 -0300

    testing

--- test/components/getProducts/getProducts.controller.spec.ts
+++ test/components/getProducts/getProducts.controller.spec.ts
@@ -2,6 +2,6 @@ import {sum} from '../../../src/components/getProducts/getProducts.controller';
 
 describe('Testing getProducts', () => {
     test('adds 1 + 2 to equal 3', () => {
-     expect(sum(1, 2)).toBe(22);
+     expect(sum(1, 2)).toBe(33);
     });
 });
