87c0e6ef923b0d20cf308a854630481cc4cc7b77
mvalen28 <mvalen28@externos.bancoestado.cl>
2022-10-23 12:38:40 -0300

    Added sonar in pipeline

--- src/utils/parseConfig.util.ts
+++ src/utils/parseConfig.util.ts
@@ -1,28 +1,3 @@
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-// const env = process.env.NODE_ENV;
-
-
 const addEnvParam = (paramName: any, required: any, format: any) => {
   let parameter: any = process.env[paramName];
 
